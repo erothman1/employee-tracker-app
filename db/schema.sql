afdd5b4bbfbf46c2577afc6bd536b4522766dc08
Elva Rothman <erothman@tulane.edu>
2023-04-20 20:30:25 -0400

    created employees table

--- db/schema.sql
+++ db/schema.sql
@@ -8,11 +8,19 @@ SELECT DATABASE();
 CREATE TABLE department (
     id INT NOT NULL,
     name VARCHAR(30) NOT NULL,
-)
+);
 
 CREATE TABLE role (
     id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
-)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+);
