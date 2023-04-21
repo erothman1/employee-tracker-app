c3f9636bf213d712566140f0bb6fd63399d65325
Elva Rothman <erothman@tulane.edu>
2023-04-20 20:29:06 -0400

    created role table

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE department (
     id INT NOT NULL,
     name VARCHAR(30) NOT NULL,
 )
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+)
