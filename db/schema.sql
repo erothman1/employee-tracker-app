3cac6f323dd7391280e5dc260d9973571249a8cd
Elva Rothman <erothman@tulane.edu>
2023-04-21 14:47:03 -0400

    created data for roles

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,10 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -22,5 +25,8 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT 
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,20 @@
 INSERT INTO department (name)
 VALUES 
-    ("Software Engineering"),
-    ("Marketing"),
-    ("Finance"),
-    ("Sales"),
-    ("Legal");
+    ("Software Engineering"), --1
+    ("Marketing"), --2
+    ("Finance"), --3
+    ("Sales"), --4
+    ("Legal"); --5
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ()
+    ("Software Engineer", 105, 1),
+    ("Lead Engineer", 150, 1),
+    ("Social Media Manager", 75, 2),
+    ("Marketing Lead", 95, 2),
+    ("Financial Analyst", 100, 3),
+    ("Lead Account Manager", 120, 3),
+    ("Sales Representative", 65, 4),
+    ("Head Sales Associate", 90, 4),
+    ("Legal Assistant", 60, 5),
+    ("Head of Legal Operations", 125, 5);
